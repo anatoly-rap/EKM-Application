8a861d6817dfcdb53491c2840a7640e8600aa69b
Anatoly <102273751+anatoly-rap@users.noreply.github.com>
2022-06-27 00:29:44 -0600

    Credentials protocols
    implemented additional credentials for logging in.  Like crypto wallets, user logins will be tied to their physical device.(MAC)

--- src/main/java/com/ar/revaes/UserAuth.java
+++ src/main/java/com/ar/revaes/UserAuth.java
@@ -1,4 +1,10 @@
 package com.ar.revaes;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Enumeration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 public class UserAuth {
@@ -13,5 +19,27 @@ public class UserAuth {
     }
     public static Boolean loginAuth(String raw, String hashed){
         return a2Encoder.matches(raw, hashed);
+    }
+    private static InetAddress userInet() throws UnknownHostException {//initial use case, will work for most standard personal/home computer's networks
+        return InetAddress.getLocalHost();
+    }
+    private static Boolean networkInterfaces() throws SocketException, UnknownHostException {   //test case for multiple interfaces,seeing if userInet() is functional(not a VM or something)
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while(interfaces.hasMoreElements()){
+            NetworkInterface networkInterface = interfaces.nextElement();
+            if(Collections.list(networkInterface.getInetAddresses()).contains(userInet()) && networkInterface.isUp()){ //check for userInet methods.
+                return true;
+                }
+            }
+            return false;
+    }
+    //need to link MAC with user credentials
+    private static byte[] getMAC(InetAddress address) throws SocketException{
+        NetworkInterface networkInterface = NetworkInterface.getByInetAddress(address);
+        return networkInterface.getHardwareAddress();
+    }
+    public static void main(String[] args) throws UnknownHostException, SocketException { //testing purposes
+        System.out.println("test ip: " + userInet());
+        System.out.println("testing Network Interfaces Test:  "+ networkInterfaces().toString());
     }
 }
